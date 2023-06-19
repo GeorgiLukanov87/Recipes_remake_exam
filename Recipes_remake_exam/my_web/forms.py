6ba135bac942e876124271d21c6319cb85ffda78
Georgi St. Lukanov <goshiii87@gmail.com>
2023-06-19 13:25:30 +0200

    .

--- Recipes_remake_exam/my_web/forms.py
+++ Recipes_remake_exam/my_web/forms.py
@@ -8,6 +8,35 @@ class BaseForm(forms.ModelForm):
         model = Recipe
         fields = '__all__'
 
+        labels = {
+            'title': 'Title',
+            'image_url': 'Image URL',
+            'description': 'Description',
+            'ingredients': 'Ingredients',
+            'time': 'Time(Minutes)',
+        }
+
 
 class CreateRecipeForm(BaseForm):
     pass
+
+
+class EditRecipeForm(BaseForm):
+    pass
+
+
+class DeleteRecipeForm(BaseForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.__disable_fields()
+
+    def save(self, commit=True):
+        if commit:
+            self.instance.delete()
+
+        return self.instance
+
+    def __disable_fields(self):
+        for field in self.fields.values():
+            field.widget.attrs['disabled'] = 'disabled'
+            field.required = False
--- Recipes_remake_exam/my_web/views.py
+++ Recipes_remake_exam/my_web/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 
-from Recipes_remake_exam.my_web.forms import CreateRecipeForm
+from Recipes_remake_exam.my_web.forms import CreateRecipeForm, EditRecipeForm, DeleteRecipeForm
 from Recipes_remake_exam.my_web.models import Recipe
 
 
@@ -27,15 +27,45 @@ def create_recipe(request):
 
     }
 
-    return render(request, 'create.html', context,)
+    return render(request, 'create.html', context, )
 
 
 def edit_recipe(request, id):
-    return render(request, 'edit.html')
+    recipe = Recipe.objects.filter(pk=id).get()
+
+    if request.method == 'GET':
+        form = EditRecipeForm(instance=recipe)
+    else:
+        form = EditRecipeForm(request.POST, instance=recipe)
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+
+    context = {
+        'form': form,
+        'recipe': recipe,
+    }
+
+    return render(request, 'edit.html', context, )
 
 
 def delete_recipe(request, id):
-    return render(request, 'delete.html')
+    recipe = Recipe.objects.filter(pk=id).get()
+
+    if request.method == 'GET':
+        form = DeleteRecipeForm(instance=recipe)
+    else:
+        form = DeleteRecipeForm(request.POST, instance=recipe)
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+
+    context = {
+        'form': form,
+        'recipe': recipe,
+    }
+
+    return render(request, 'delete.html', context, )
 
 
 def details_recipe(request, id):
